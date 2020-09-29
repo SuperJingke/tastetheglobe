86d11537018090f3b82d1bc505b19c8e2b7dc193
Gillian Liang <Veladaraston@Gillians-MacBook-Pro.local>
2020-09-29 16:48:05 -0400

    implement jquery and bootstramp

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/slider-image-1.jpg',
+
+
+
+			 'images/slider-image-2.jpg']
+
+
+
+
+
+
+
+});
---
+++ js/countdown-js.js
@@ -0,0 +1,31 @@
+// set the date we're counting down to
+var target_date = new Date('Jan, 31, 2018').getTime();
+ 
+// variables for time units
+var days, hours, minutes, seconds;
+ 
+// get tag element
+var countdown = document.getElementById('countdown');
+ 
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value
+    countdown.innerHTML = '<span class="days">' + days +  ' <b>Days</b></span> <span class="hours">' + hours + ' <b>Hours</b></span> <span class="minutes">'
+    + minutes + ' <b>Minutes</b></span> <span class="seconds">' + seconds + ' <b>Seconds</b></span>';  
+ 
+}, 1000);
